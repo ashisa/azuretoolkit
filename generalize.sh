f1b17bd8aa3f370bd37e57c28cd54289e2474c3c
Ashish Sahu <ashisa@live.com>
2016-11-15 15:03:24 +0530

    Removing +user from the script
    Removing +user from the script

--- generalize.sh
+++ generalize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "deprovisioning the azure agent..."
-/usr/bin/python3 /usr/sbin/waagent -deprovision+user -force
+/usr/bin/python3 /usr/sbin/waagent -deprovision -force
 echo "deprovisioning over. VM ready to generalize and capture."
