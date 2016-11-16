e6428583f90fd997b4c2304c1672d698e55835ca
Ashish Sahu <ashisa@live.com>
2016-11-16 19:47:05 +0530

    Playing with crontab now
    Playing with crontab now

--- generalize.sh
+++ generalize.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "deprovisioning the azure agent..."
+echo '* * * * * /tmp/deprovision.sh >/tmp/deprovision.log 2>&1' >/tmp/cronjob.tmp
 echo 'sudo waagent -deprovision+user -force; sudo shutdown -h now' >/tmp/deprovision.sh
 chmod a+x /tmp/deprovision.sh
+crontab /tmp/cronjob.tmp
 echo "deprovisioning over. VM ready to generalize and capture."
