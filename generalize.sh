f28761ad05f2116474aef62eced17b8fe43e35d7
Ashish Sahu <ashisa@live.com>
2016-11-16 21:28:56 +0530

    bringing back crontab
    bringing back crontab

--- generalize.sh
+++ generalize.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "deprovisioning the azure agent..."
-#echo '* * * * * /tmp/deprovision.sh >/tmp/deprovision.log 2>&1' >/tmp/cronjob.tmp
-echo 'sudo waagent -deprovision+user -force; sudo shutdown -h now' >/tmp/deprovision.sh
+echo '* * * * * /tmp/deprovision.sh >/tmp/deprovision.log 2>&1' >/tmp/cronjob.tmp
+echo 'sudo waagent -deprovision+user -force; crontab -r; sudo shutdown -h now' >/tmp/deprovision.sh
 chmod a+x /tmp/deprovision.sh
-(sleep 30; sudo /tmp/deprovision.sh >/tmp/deprovision.log 2>&1; sudo shutdown -h now) &
-#crontab /tmp/cronjob.tmp
+crontab /tmp/cronjob.tmp
 echo "deprovisioning over. VM ready to generalize and capture."
