6e2d35e4b9e2b998d98507d974686340cded820a
Ashish Sahu <ashisa@live.com>
2016-11-16 21:15:34 +0530

    Testing background again
    Testing background again

--- generalize.sh
+++ generalize.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "deprovisioning the azure agent..."
-echo '* * * * * /tmp/deprovision.sh >/tmp/deprovision.log 2>&1' >/tmp/cronjob.tmp
+#echo '* * * * * /tmp/deprovision.sh >/tmp/deprovision.log 2>&1' >/tmp/cronjob.tmp
 echo 'sudo waagent -deprovision+user -force; sudo shutdown -h now' >/tmp/deprovision.sh
 chmod a+x /tmp/deprovision.sh
-crontab /tmp/cronjob.tmp
+(sleep 30; sudo /tmp/deprovision.sh >/tmp/deprovision.log 2>&1; sudo shutdown -h now) &
+#crontab /tmp/cronjob.tmp
 echo "deprovisioning over. VM ready to generalize and capture."
