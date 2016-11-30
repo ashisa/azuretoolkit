5dc4df75188382889b49ec5f359e0ba76699c04e
Ashish Sahu <ashisa@live.com>
2016-11-30 14:22:24 +0530

    Adding sleep to the cron entry
    Adding sleep to the cron entry

--- generalize.sh
+++ generalize.sh
@@ -2,7 +2,7 @@
 
 echo "deprovisioning the azure agent..."
 echo '* * * * * /tmp/deprovision.sh >/tmp/deprovision.log 2>&1' >/tmp/cronjob.tmp
-echo 'sudo waagent -deprovision+user -force; crontab -r; sudo shutdown -h now' >/tmp/deprovision.sh
+echo 'SLEEP 60; sudo waagent -deprovision+user -force; crontab -r' >/tmp/deprovision.sh
 chmod a+x /tmp/deprovision.sh
 crontab /tmp/cronjob.tmp
 echo "deprovisioning over. VM ready to generalize and capture."
