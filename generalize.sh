eebb41b65ab4bbc449d798ea94d66493ff077630
Ashish Sahu <ashisa@live.com>
2016-11-30 15:29:16 +0530

    Sleep 180
    Sleep 180

--- generalize.sh
+++ generalize.sh
@@ -2,7 +2,7 @@
 
 echo "deprovisioning the azure agent..."
 echo '* * * * * /tmp/deprovision.sh >/tmp/deprovision.log 2>&1' >/tmp/cronjob.tmp
-echo 'sleep 120; sudo waagent -deprovision+user -force; crontab -r' >/tmp/deprovision.sh
+echo 'sleep 180; sudo waagent -deprovision+user -force; crontab -r' >/tmp/deprovision.sh
 chmod a+x /tmp/deprovision.sh
 crontab /tmp/cronjob.tmp
 echo "deprovisioning over. VM ready to generalize and capture."
